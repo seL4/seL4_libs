0c8197a1e47c1bfeb909c428b1c957a35389aa08
Adrian Danis <Adrian.Danis@nicta.com.au>
2014-09-09 12:12:39 +1000

    Add set_thread_area and set_tid_address syscalls

--- src/sys_stubs.c
+++ src/sys_stubs.c
@@ -1195,11 +1195,6 @@ long sys_sched_getaffinity(va_list ap)
     assert(!"sys_sched_getaffinity not implemented");
     return 0;
 }
-long sys_set_thread_area(va_list ap)
-{
-    assert(!"sys_set_thread_area not implemented");
-    return 0;
-}
 long sys_get_thread_area(va_list ap)
 {
     assert(!"sys_get_thread_area not implemented");
@@ -1263,11 +1258,6 @@ long sys_epoll_wait(va_list ap)
 long sys_remap_file_pages(va_list ap)
 {
     assert(!"sys_remap_file_pages not implemented");
-    return 0;
-}
-long sys_set_tid_address(va_list ap)
-{
-    assert(!"sys_set_tid_address not implemented");
     return 0;
 }
 long sys_timer_create(va_list ap)
@@ -2728,11 +2718,6 @@ long sys_epoll_wait(va_list ap)
 long sys_remap_file_pages(va_list ap)
 {
     assert(!"sys_remap_file_pages not implemented");
-    return 0;
-}
-long sys_set_tid_address(va_list ap)
-{
-    assert(!"sys_set_tid_address not implemented");
     return 0;
 }
 long sys_timer_create(va_list ap)

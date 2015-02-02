72117ed00d11de18f8cb85569060385c8c4590b7
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2015-02-02 18:01:14 +1100

    Add some missing arch-independence macros.

--- include/sel4utils/mapping.h
+++ include/sel4utils/mapping.h
@@ -32,7 +32,9 @@ static int UNUSED sel4_supported_page_sizes[] = {12, 16, 20, 24};
 #define seL4_ARCH_Page_GetAddress      seL4_ARM_Page_GetAddress
 #define seL4_ARCH_Page_GetAddress_t    seL4_ARM_Page_GetAddress_t
 #define seL4_ARCH_PageTable_Map        seL4_ARM_PageTable_Map
+#define seL4_ARCH_PageTable_Unmap      seL4_ARM_PageTable_Unmap
 #define seL4_ARCH_ASIDPool_Assign      seL4_ARM_ASIDPool_Assign
+#define seL4_ARCH_ASIDControl_MakePool seL4_ARM_ASIDControl_MakePool
 #define seL4_ARCH_PageTableObject      seL4_ARM_PageTableObject
 #define seL4_ARCH_PageDirectoryObject  seL4_ARM_PageDirectoryObject
 #define seL4_ARCH_Default_VMAttributes seL4_ARM_Default_VMAttributes
@@ -68,7 +70,9 @@ static int UNUSED sel4_supported_page_sizes[] = {12, 22};
 #define seL4_ARCH_Page_GetAddress      seL4_IA32_Page_GetAddress
 #define seL4_ARCH_Page_GetAddress_t    seL4_IA32_Page_GetAddress_t
 #define seL4_ARCH_PageTable_Map        seL4_IA32_PageTable_Map
+#define seL4_ARCH_PageTable_Unmap      seL4_IA32_PageTable_Unmap
 #define seL4_ARCH_ASIDPool_Assign      seL4_IA32_ASIDPool_Assign
+#define seL4_ARCH_ASIDControl_MakePool seL4_IA32_ASIDControl_MakePool
 #define seL4_ARCH_PageTableObject      seL4_IA32_PageTableObject
 #define seL4_ARCH_PageDirectoryObject  seL4_IA32_PageDirectoryObject
 #define seL4_ARCH_Default_VMAttributes seL4_IA32_Default_VMAttributes

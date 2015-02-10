e8d99b2c3bc4a637a24ecdedf0c4302e77ed0238
Adrian Danis <Adrian.Danis@nicta.com.au>
2015-02-11 09:49:36 +1100

    Implement code to examine the stack frame and to print a stack trace
    This uses gcc builtins that may or may not be implemented on a given
    architecture and which may or may not explode when used. This is
    designed as a quick and dirty solution to getting a stack trace if
    your code is reaching a place you feel it shouldn't (such as an
    error path).

---
+++ include/sel4debug/stack_trace.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+
+#ifndef _LIBSEL4DEBUG_STACK_TRACE_H_
+#define _LIBSEL4DEBUG_STACK_TRACE_H_
+
+void print_stack_trace(void);
+
+#endif /* !_LIBSEL4DEBUG_STACK_TRACE_H_ */

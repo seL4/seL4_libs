662529faff8f4143dd8277e38b91dfd3eca57b48
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2015-03-18 11:19:30 +1100

    Remove confusing and incorrect comments and structure.

--- include/sel4utils/sel4_debug.h
+++ include/sel4utils/sel4_debug.h
@@ -8,16 +8,10 @@
  * @TAG(NICTA_BSD)
  */
 
-/*
- * <assert.h> may safely be included multiple times with or without NDEBUG
- */
-#undef sel4_error
-
-#define	sel4_error(e, str)	((e == seL4_NoError) ? (void)0 : __sel4_error(e, __FILE__, __func__, __LINE__, str))
-
-/* Implemented in src/assert.c */
 #ifndef _SEL4_DEBUG_H_
 #define _SEL4_DEBUG_H_
+
+#define	sel4_error(e, str)	((e == seL4_NoError) ? (void)0 : __sel4_error(e, __FILE__, __func__, __LINE__, str))
 
 void __sel4_error(int, const char *, const char *, int, char *);
 

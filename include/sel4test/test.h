c0bbffdeae8337455bd6e09191d7c2d39b5b4927
Anna Lyons <Anna.Lyons@nicta.com.au>
2015-07-09 14:19:26 +1000

    add pretty printed macro wrappers for test_check that output the values of variables on failure

--- include/sel4test/test.h
+++ include/sel4test/test.h
@@ -19,6 +19,9 @@
 #include <sel4test/prototype.h>
 #include <sel4test/macros.h>
 
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -108,6 +111,85 @@ static inline void _test_abort(char *condition, char *file, int line)
 #define test_assert(e) if (!(e)) return _test_fail(#e, __FILE__, __LINE__)
 #define test_check(e) if (!(e)) _test_error(#e, __FILE__, __LINE__)
 #define test_assert_fatal(e) if (!(e)) _test_abort(#e, __FILE__, __LINE__)
+
+#define __TEST_BUFFER_SIZE 200
+#define test_op_type(a, b, op, t, name_a, name_b, cast) \
+    do {\
+        if (!(a op b)) { \
+            int len = snprintf(NULL, 0, "Check %s("t") %s %s("t") failed.",\
+                               #name_a, (cast) a, #op, #name_b, (cast) b) + 1; \
+            char buffer[len]; \
+            snprintf(buffer, len, "Check %s("t") %s %s("t") failed.",\
+                     #name_a, (cast) a, #op, #name_b, (cast) b); \
+            _test_error(buffer, __FILE__, __LINE__); \
+        }\
+    } while (0)
+
+#define test_op(a, b, op) \
+    do { \
+         typeof (a) _a = (a); \
+         typeof (b) _b = (b); \
+         if (sizeof(_a) != sizeof(_b)) { \
+             int len = snprintf(NULL, 0, #a" (size %d) != "#b" (size %d), use of test_eq incorrect",\
+                     sizeof(_a), sizeof(_b)) + 1;\
+             char buffer[len];\
+             snprintf(buffer, len, #a" (size %d) != "#b" (size %d), use of test_eq incorrect", sizeof(_a),\
+                     sizeof(_b));\
+             _test_error(buffer, __FILE__, __LINE__);\
+         } else if (TYPES_COMPATIBLE(typeof(_a), int)) {\
+             test_op_type(_a, _b, op, "%d", a, b, int); \
+         } else if (TYPES_COMPATIBLE(typeof(_a), long)) {\
+             test_op_type(_a, _b, op, "%ld", a, b, long); \
+         } else if (TYPES_COMPATIBLE(typeof(_a), long long)) {\
+             test_op_type(_a, _b, op, "%lld", a, b, long long); \
+         } else if (TYPES_COMPATIBLE(typeof(_a), unsigned int)) {\
+             test_op_type(_a, _b, op, "%u", a, b, unsigned int); \
+         } else if (TYPES_COMPATIBLE(typeof(_a), unsigned long)) {\
+             test_op_type(_a, _b, op, "%lu", a, b, unsigned long); \
+         } else if (TYPES_COMPATIBLE(typeof(_a), unsigned long long)) {\
+             test_op_type(_a, _b, op, "%llu", a, b, unsigned long long); \
+         } else if (TYPES_COMPATIBLE(typeof(_a), char)) {\
+             test_op_type(_a, _b, op, "%c", a, b, char); \
+         } else if (TYPES_COMPATIBLE(typeof(_a), uintptr_t)) {\
+             test_op_type(_a, _b, op, "0x%"PRIxPTR, a, b, uintptr_t);\
+         } else { \
+             _test_error("Cannot use test_op on this type", __FILE__, __LINE__);\
+         }\
+    } while (0)
+
+/* Pretty printed test_check wrapper macros for basic comparisons on base types,
+ * which output the values and variable names to aid debugging */
+#define test_eq(a, b)  test_op(a, b, ==)
+#define test_neq(a, b) test_op(a, b, !=)
+#define test_ge(a, b)  test_op(a, b, >)
+#define test_geq(a, b) test_op(a, b, >=)
+#define test_le(a, b)  test_op(a, b, <)
+#define test_leq(a, b) test_op(a, b, <=)
+
+#define __TEST_MAX_STRING 50
+#define test_strop(a, b, op) \
+     do {\
+          if (strnlen(a, __TEST_MAX_STRING) == __TEST_MAX_STRING) {\
+              _test_error("String "#a" too long for test_str* macros", __FILE__, __LINE__);\
+          } else if (strnlen(b, __TEST_MAX_STRING) == __TEST_MAX_STRING) {\
+              _test_error("String "#b" too long for test_str* macros", __FILE__, __LINE__);\
+          } else if (!(strncmp(a, b, __TEST_MAX_STRING)) op 0) {\
+              char buffer[__TEST_BUFFER_SIZE + 2 * __TEST_MAX_STRING];\
+              snprintf(buffer, sizeof(buffer),\
+                       "Check %s(%s) %s %s(%s) failed.", #a, a, #op, #b, b);\
+              _test_error(buffer, __FILE__, __LINE__); \
+          }\
+    } while (0)
+
+/* Pretty printed test_check wrapper macros for basic comparisons on c strings, 
+ * which output the values and variable names to aid debugging */
+#define test_streq(a, b)  test_strop(a, b, ==)
+#define test_strneq(a, b) test_strop(a, b, !=)
+#define test_strge(a, b)  test_strop(a, b, >)
+#define test_strgeq(a, b) test_strop(a, b, >=)
+#define test_strle(a, b)  test_strop(a, b, <)
+#define test_strleq(a, b) test_strop(a, b, <=)
+
 
 env_t sel4test_get_env(void);
 

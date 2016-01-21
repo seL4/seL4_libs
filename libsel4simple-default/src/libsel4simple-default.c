9c9f0896da2ee8d8e5dd541d8aa4daac3d862252
Adrian Danis <Adrian.Danis@nicta.com.au>
2016-01-21 15:58:54 +1100

    libsel4simple-default: Attempt to support getting interrupts when using the IOAPIC

--- libsel4simple-default/src/libsel4simple-default.c
+++ libsel4simple-default/src/libsel4simple-default.c
@@ -76,7 +76,33 @@ void *simple_default_get_frame_mapping(void *data, void *paddr, int size_bits) {
 }
 
 seL4_Error simple_default_get_irq(void *data, int irq, seL4_CNode root, seL4_Word index, uint8_t depth) {
+#ifdef CONFIG_IRQ_IOAPIC
+    /* we need to try and guess how to map a requested IRQ to an IOAPIC
+     * pin, as well as what the edge and polarity detection mode is.
+     * Without any way to inspect the ACPI tables here we make the following
+     * assumptions
+     *   + There is an override for ISA-IRQ-0 -> GSI 2
+     *   + Only one IOAPIC and error on IRQs >= 24
+     *   + IRQs below 16 are ISA and edge detected polarity high
+     *   + IRQs 16 and above are PCI and level detected polarity low
+     */
+    int vector = irq;
+    if (irq == 0) {
+        irq = 2;
+    }
+    int level;
+    int low_polarity;
+    if (irq >= 16) {
+        level = 1;
+        low_polarity = 1;
+    } else {
+        level = 0;
+        low_polarity = 0;
+    }
+    return seL4_IRQControl_GetIOAPIC(seL4_CapIRQControl, root, index, depth, 0, irq, level, low_polarity, vector);
+#else
     return seL4_IRQControl_Get(seL4_CapIRQControl, irq, root, index, depth);
+#endif
 }
 
 seL4_Error simple_default_set_ASID(void *data, seL4_CPtr vspace) {

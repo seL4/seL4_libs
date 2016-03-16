36ea8cc48fa72f7929c5a4ce10c1a4a780582fbe
Anna Lyons <Anna.Lyons@nicta.com.au>
2016-03-16 17:00:01 +1100

    libsel4platsupport: absolute & relative omap timer

--- libsel4platsupport/mach_include/omap/sel4platsupport/mach/gpt.h
+++ libsel4platsupport/mach_include/omap/sel4platsupport/mach/gpt.h
@@ -34,13 +34,36 @@ typedef timer_common_data_t gpt_data;
 /**
  * Get an interface for an initialised gpt timer.
  *
+ * @deprecated use sel4platsupport_get_rel_gpt
  * @param notification notification object for the irq to come in on
  * @param prescaler to scale time by. 0 = divide by 1. 1 = divide by 2, ...
  * @param gpt_id some platforms have more than one gpt, pick one.
  */
+DEPRECATED("use sel4platsupport_get_rel_gpt")
 seL4_timer_t *sel4platsupport_get_gpt(vspace_t *vspace, simple_t *simple, vka_t *vka,
                                       seL4_CPtr notification, gpt_id_t id, uint32_t prescaler);
-
+/** 
+ * Get a GPT timer driver that supports relative and periodic timeouts.
+ *
+ * @param notification notification object for the irq to come in on
+ * @param prescaler to scale time by. 0 = divide by 1. 1 = divide by 2, ...
+ * @param gpt_id some platforms have more than one gpt, pick one.
+ */
+seL4_timer_t * 
+sel4platsupport_get_rel_gpt(vspace_t *vspace, simple_t *simple, vka_t *vka, seL4_CPtr notification,
+                            gpt_id_t gpt_id, uint32_t prescaler);
+/** 
+ * Get a GPT timer driver that absolute and relative one shot timeouts, and 
+ * tracks a 64bit current time.
+ *
+ * @param notification notification object for the irq to come in on
+ * @param prescaler to scale time by. 0 = divide by 1. 1 = divide by 2, ...
+ * @param gpt_id some platforms have more than one gpt, pick one.
+ */
+seL4_timer_t *
+sel4platsupport_get_abs_gpt(vspace_t *vspace, simple_t *simple, vka_t *vka, seL4_CPtr notification, 
+                            gpt_id_t gpt_id, uint32_t prescaler);
+              
 void sel4platsupport_destroy_gpt(seL4_timer_t *timer, vka_t *vka, vspace_t *vspace);
 
 #endif /* __SEL4_PLAT_SUPPORT_GPT_H */

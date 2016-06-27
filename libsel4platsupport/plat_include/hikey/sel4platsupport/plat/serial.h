d4c8f6fc8d96047ae76d840686eafcdb9c83742d
amrzar <azarrabi@nicta.com.au>
2016-06-27 14:13:57 +1000

    add missing file

---
+++ libsel4platsupport/plat_include/hikey/sel4platsupport/plat/serial.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2016, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ */
+#pragma once
+
+#include <platsupport/plat/serial.h>
+
+#define DEFAULT_SERIAL_PADDR UART0_PADDR
+#define DEFAULT_SERIAL_INTERRUPT UART0_IRQ
+

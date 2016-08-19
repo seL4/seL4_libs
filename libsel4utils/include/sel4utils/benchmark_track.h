af3391de729318e6d1138c1e3105d098f3b1cf7e
Hesham Almatary <hesham.almatary@data61.csiro.au>
2016-08-19 15:59:44 +1000

    Benchmark Track - Delete usage of dump and get size system calls

--- libsel4utils/include/sel4utils/benchmark_track.h
+++ libsel4utils/include/sel4utils/benchmark_track.h
@@ -17,11 +17,9 @@
 
 /* Print out a summary of what has been tracked */
 static inline void
-seL4_BenchmarkTrackDumpSummary(void)
+seL4_BenchmarkTrackDumpSummary(benchmark_track_kernel_entry_t *logBuffer, size_t logSize)
 {
-    seL4_Word start_index = 0;
-    seL4_Word remaining_entries = 0;
-    seL4_Word received_entries = 0;
+    seL4_Word index = 0;
     seL4_Word syscall_entries = 0;
     seL4_Word interrupt_entries = 0;
     seL4_Word userlevelfault_entries = 0;
@@ -32,62 +30,31 @@ seL4_BenchmarkTrackDumpSummary(void)
      */
     FILE *fd = stdout;
 
-    /* IPC buffer is used to hold log data from the kernel */
-    benchmark_track_kernel_entry_t *ipcBuffer = (benchmark_track_kernel_entry_t *)
-                                                & (seL4_GetIPCBuffer()->msg[0]);
-
-    /* Get the total number of kernel entries between a call to seL4_BenchmarkResetLog and
-     * seL4_BenchmarkFinalizeLog. Note, this might be limited by the kernel log size,
-     * once it get filled, it stops tracking silently.
-     */
-    remaining_entries = (seL4_Uint32) seL4_BenchmarkLogSize();
-
-    /* Get number of invocations on each cap */
-    fprintf(fd, "---------------------------------------------------------------------------------------------------------\n");
-    fprintf(fd, "| The kernel logged %d entries\n", remaining_entries);
-    fprintf(fd, "---------------------------------------------------------------------------------------------------------\n");
-
-    /* Copy the kernel logged data to the IPC Buffer, and dump it. This operation will be repeated
-     * many times depending on both the kernel log and IPC Buffer sizes.
-     */
-    while (remaining_entries) {
-        received_entries = seL4_BenchmarkDumpLog(start_index, remaining_entries);
-
-        for (int i = 0; i < received_entries; i++) {
-            if (ipcBuffer[i].entry.path == Entry_Syscall) {
-                syscall_entries++;
-            } else if (ipcBuffer[i].entry.path == Entry_Interrupt) {
-                interrupt_entries++;
-            } else if (ipcBuffer[i].entry.path == Entry_UserLevelFault) {
-                userlevelfault_entries++;
-            } else if (ipcBuffer[i].entry.path == Entry_VMFault) {
-                vmfault_entries++;
-            }
+    while (logBuffer[index].start_time != 0 && (index + 1) * sizeof(benchmark_track_kernel_entry_t) <= logSize) {
+        if (logBuffer[index].entry.path == Entry_Syscall) {
+            syscall_entries++;
+        } else if (logBuffer[index].entry.path == Entry_Interrupt) {
+            interrupt_entries++;
+        } else if (logBuffer[index].entry.path == Entry_UserLevelFault) {
+            userlevelfault_entries++;
+        } else if (logBuffer[index].entry.path == Entry_VMFault) {
+            vmfault_entries++;
         }
-
-        start_index += received_entries;
-        remaining_entries -= received_entries;
-
-    };
+        index++;
+    }
 
     fprintf(fd, "Number of system call invocations %d\n", syscall_entries);
     fprintf(fd, "Number of interrupt invocations %d\n", interrupt_entries);
     fprintf(fd, "Number of user-level faults %d\n", userlevelfault_entries);
     fprintf(fd, "Number of VM faults %d\n", vmfault_entries);
-
 }
 
 /* Print out logged system call invocations */
 static inline void
-seL4_BenchmarkTrackDumpFullSyscallLog(void)
+seL4_BenchmarkTrackDumpFullSyscallLog(benchmark_track_kernel_entry_t *logBuffer, size_t logSize)
 {
-    seL4_Word start_index = 0;
-    seL4_Word remaining_entries = 0;
-    seL4_Word received_entries = 0;
+    seL4_Word index = 0;
     FILE *fd = stdout;
-
-    benchmark_track_kernel_entry_t *ipcBuffer = (benchmark_track_kernel_entry_t *)
-                                                & (seL4_GetIPCBuffer()->msg[0]);
 
     /* Get details of each system call invocation */
     fprintf(fd, "-----------------------------------------------------------------------------------------------------------------------------\n");
@@ -96,41 +63,27 @@ seL4_BenchmarkTrackDumpFullSyscallLog(void)
             "Invocation Tag",  "Fastpath?");
     fprintf(fd, "-----------------------------------------------------------------------------------------------------------------------------\n");
 
-    remaining_entries = (seL4_Uint32) seL4_BenchmarkLogSize();
-
-    while (remaining_entries) {
-        received_entries = seL4_BenchmarkDumpLog(start_index, remaining_entries);
-
-        for (int i = 0; i < received_entries; i++) {
-            if (ipcBuffer[i].entry.path == Entry_Syscall) {
-                fprintf(fd, "|     %-15d|     %-15d|     %-15llu|     %-15d|     %-15d|     %-15d|     %-15d|\n",
-                        i + start_index,
-                        ipcBuffer[i].entry.syscall_no,
-                        (uint64_t ) ipcBuffer[i].start_time,
-                        ipcBuffer[i].duration,
-                        ipcBuffer[i].entry.cap_type,
-                        ipcBuffer[i].entry.invocation_tag,
-                        ipcBuffer[i].entry.is_fastpath);
-            }
+    while (logBuffer[index].start_time != 0 && (index + 1) * sizeof(benchmark_track_kernel_entry_t) <= logSize) {
+        if (logBuffer[index].entry.path == Entry_Syscall) {
+            fprintf(fd, "|     %-15d|     %-15d|     %-15llu|     %-15d|     %-15d|     %-15d|     %-15d|\n",
+                    index,
+                    logBuffer[index].entry.syscall_no,
+                    (uint64_t ) logBuffer[index].start_time,
+                    logBuffer[index].duration,
+                    logBuffer[index].entry.cap_type,
+                    logBuffer[index].entry.invocation_tag,
+                    logBuffer[index].entry.is_fastpath);
         }
-
-        start_index += received_entries;
-        remaining_entries -= received_entries;
-
-    };
+        index++;
+    }
 }
 
 /* Print out logged interrupt invocations */
 static inline void
-seL4_BenchmarkTrackDumpFullInterruptLog(void)
+seL4_BenchmarkTrackDumpFullInterruptLog(benchmark_track_kernel_entry_t *logBuffer, size_t logSize)
 {
-    seL4_Word start_index = 0;
-    seL4_Word remaining_entries = 0;
-    seL4_Word received_entries = 0;
+    seL4_Word index = 0;
     FILE *fd = stdout;
-
-    benchmark_track_kernel_entry_t *ipcBuffer = (benchmark_track_kernel_entry_t *)
-                                                & (seL4_GetIPCBuffer()->msg[0]);
 
     /* Get details of each invocation */
     fprintf(fd, "-----------------------------------------------------------------------------------------------------------------------------\n");
@@ -138,25 +91,15 @@ seL4_BenchmarkTrackDumpFullInterruptLog(void)
             "Duration");
     fprintf(fd, "-----------------------------------------------------------------------------------------------------------------------------\n");
 
-    remaining_entries = (seL4_Uint32) seL4_BenchmarkLogSize();
-
-    while (remaining_entries) {
-        received_entries = seL4_BenchmarkDumpLog(start_index, remaining_entries);
-
-        for (int i = 0; i < received_entries; i++) {
-            if (ipcBuffer[i].entry.path == Entry_Interrupt) {
-                fprintf(fd, "|     %-15d|     %-15d|     %-15llu|     %-15d|\n", \
-                        i + start_index,
-                        ipcBuffer[i].entry.word,
-                        ipcBuffer[i].start_time,
-                        ipcBuffer[i].duration);
-            }
+    while (logBuffer[index].start_time != 0 && (index + 1) * sizeof(benchmark_track_kernel_entry_t) <= logSize) {
+        if (logBuffer[index].entry.path == Entry_Interrupt) {
+            fprintf(fd, "|     %-15d|     %-15d|     %-15llu|     %-15d|\n", \
+                    index,
+                    logBuffer[index].entry.word,
+                    logBuffer[index].start_time,
+                    logBuffer[index].duration);
         }
-
-        start_index += received_entries;
-        remaining_entries -= received_entries;
-
-    };
-
+        index++;
+    }
 }
 #endif /* CONFIG_BENCHMARK_TRACK_KERNEL_ENTRIES */

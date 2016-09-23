c3747874fc2f2ed6857349d07fde4a17a33c1e0f
Adrian Danis <Adrian.Danis@data61.csiro.au>
2016-09-23 15:44:41 +1000

    libsel4debug: Add missing copyright header

--- libsel4debug/arch_include/arm/sel4debug/arch/registers.h
+++ libsel4debug/arch_include/arm/sel4debug/arch/registers.h
@@ -1,3 +1,14 @@
+/*
+ * Copyright 2016, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(D61_BSD)
+ */
 
 #ifndef __DEBUG_ARCH_REGISTERS_H
 #define __DEBUG_ARCH_REGISTERS_H

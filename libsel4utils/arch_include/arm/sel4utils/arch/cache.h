f95c61567a69c96f9676cb15ae337aec0dc6c136
amrzar <azarrabi@nicta.com.au>
2017-01-24 12:38:08 +1100

    libsel4utils: Add aarch64 support

--- libsel4utils/arch_include/arm/sel4utils/arch/cache.h
+++ libsel4utils/arch_include/arm/sel4utils/arch/cache.h
@@ -10,22 +10,7 @@
 #ifndef SEL4UTILS_ARCH_CACHE_H
 #define SEL4UTILS_ARCH_CACHE_H
 
-#include <sel4/sel4.h>
-
-static inline int seL4_ARCH_PageDirectory_Clean_Data(seL4_CPtr root, seL4_Word start, seL4_Word end)
-{
-    return seL4_ARM_PageDirectory_Clean_Data(root, start, end);
-}
-
-static inline int seL4_ARCH_PageDirectory_Invalidate_Data(seL4_CPtr root, seL4_Word start, seL4_Word end)
-{
-    return seL4_ARM_PageDirectory_Invalidate_Data(root, start, end);
-}
-
-static inline int seL4_ARCH_PageDirectory_CleanInvalidate_Data(seL4_CPtr root, seL4_Word start, seL4_Word end)
-{
-    return seL4_ARM_PageDirectory_CleanInvalidate_Data(root, start, end);
-}
+#include <sel4utils/sel4_arch/cache.h>
 
 #endif /* SEL4UTILS_ARCH_CACHE_H */
 
--- libsel4utils/src/sel4_arch/aarch64/arch.c
+++ libsel4utils/src/sel4_arch/aarch64/arch.c
@@ -9,3 +9,34 @@
  *
  * @TAG(D61_BSD)
  */
+
+#include <autoconf.h>
+#include <sel4/types.h>
+#include <sel4utils/thread.h>
+#include <sel4utils/helpers.h>
+#include <stdbool.h>
+
+int
+sel4utils_arch_init_context(void *entry_point, void *stack_top, seL4_UserContext *context)
+{
+    context->pc = (seL4_Word) entry_point;
+    context->sp = (seL4_Word) stack_top;
+    if ((uintptr_t) stack_top % (sizeof(seL4_Word) * 2) != 0) {
+        ZF_LOGE("Stack %p not aligned on double word boundary", stack_top);
+        return -1;
+    }
+    return 0;
+}
+
+int
+sel4utils_arch_init_context_with_args(void *entry_point, void *arg0, void *arg1, void *arg2,
+                            bool local_stack, void *stack_top, seL4_UserContext *context,
+                            vka_t *vka, vspace_t *local_vspace, vspace_t *remote_vspace)
+{
+
+    context->x0 = (seL4_Word) arg0;
+    context->x1 = (seL4_Word) arg1;
+    context->x2 = (seL4_Word) arg2;
+
+    return sel4utils_arch_init_context(entry_point, stack_top, context);
+}

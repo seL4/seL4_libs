2054bbefd3c52198caac422931eeda53ae32132a
Adrian Danis <Adrian.Danis@data61.csiro.au>
2017-01-27 15:21:52 +1100

    libsel4muslcsys: Allow syscalls to be overriden at run time
    Adds an interface to override syscalls at run time. For syscalls that happen
    before a run time handler can be installed an interface is also provided for
    querying the attempted invocation.

---
+++ libsel4muslcsys/include/muslcsys/vsyscall.h
@@ -0,0 +1,37 @@
+/*
+ *  Copyright 2017, Data61
+ *  Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ *  ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(D61_BSD)
+ */
+
+#pragma once
+
+#include <autoconf.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <bits/syscall.h>
+#include <utils/attribute.h>
+
+typedef long (*muslcsys_syscall_t)(va_list);
+
+/* Installs a new handler for the given syscall. Any previous handler is returned
+ * and can be used to provide layers of syscall handling */
+muslcsys_syscall_t muslcsys_install_syscall(int syscall, muslcsys_syscall_t new_syscall);
+
+/* Some syscalls happen in the C library prior the init constructors being called,
+ * and hence prior to syscall handlers being able to be installed. For such cases
+ * we simply record such occurances and provide functions here for retrieving the
+ * arguments allow a process to do anything necessary once they get to `main`.
+ * These functions return true/false if the syscall was called on boot, and take
+ * pointers for returning the arguments. Pointers must be non NULL
+ */
+bool muslcsys_get_boot_set_tid_address(int **arg) NONNULL(1);
+#ifdef __NR_set_thread_area
+bool muslcsys_get_boot_set_thread_area(void **arg) NONNULL(1);
+#endif
--- libsel4muslcsys/src/vsyscall.c
+++ libsel4muslcsys/src/vsyscall.c
@@ -15,14 +15,61 @@
 #include <utils/util.h>
 #include <bits/syscall.h>
 #include <bits/errno.h>
+#include <muslcsys/vsyscall.h>
 #include "syscalls.h"
 
-/* construct a lookup table of all the syscalls */
-static long (*syscall_table[])(va_list) = {
+#define MUSLC_NUM_SYSCALLS (MUSLC_HIGHEST_SYSCALL + 1)
+
+/* For each TLS syscalls we record up to one occurance here that happens
+ * on startup before the syscall table is initialized. In the case of more
+ * than one occurance we will panic */
+static bool boot_set_tid_address_happened;
+static int *boot_set_tid_address_arg;
+
 #ifdef __NR_set_thread_area
-    [__NR_set_thread_area] = sys_set_thread_area,
+static bool boot_set_thread_area_happened;
+static void *boot_set_thread_area_arg;
+
+static long boot_set_thread_area(va_list ap) {
+    void *tp = va_arg(ap, void*);
+    if (boot_set_thread_area_happened) {
+        ZF_LOGE("Boot version of set_thread_area somehow got called twice");
+        return -ESRCH;
+    }
+    boot_set_thread_area_happened = true;
+    boot_set_thread_area_arg = tp;
+    return 0;
+}
+
+bool muslcsys_get_boot_set_thread_area(void **arg) {
+    assert(arg);
+    *arg = boot_set_thread_area_arg;
+    return boot_set_thread_area_happened;
+}
 #endif
-    [__NR_set_tid_address] = sys_set_tid_address,
+
+static long boot_set_tid_address(va_list ap) {
+    int *tid = va_arg(ap, int*);
+    if (boot_set_tid_address_happened) {
+        ZF_LOGE("Boot version of set_tid_address somehow got called twice");
+        return 1;
+    }
+    boot_set_tid_address_happened = true;
+    boot_set_tid_address_arg = tid;
+    return 1;
+}
+
+bool muslcsys_get_boot_set_tid_address(int **arg) {
+    assert(arg);
+    *arg = boot_set_tid_address_arg;
+    return boot_set_tid_address_happened;
+}
+
+static muslcsys_syscall_t syscall_table[MUSLC_NUM_SYSCALLS] = {
+#ifdef __NR_set_thread_area
+    [__NR_set_thread_area] = boot_set_thread_area,
+#endif
+    [__NR_set_tid_address] = boot_set_tid_address,
     [__NR_writev] = sys_writev,
     [__NR_sched_yield] = sys_sched_yield,
     [__NR_exit] = sys_exit,
@@ -60,6 +107,30 @@ static long (*syscall_table[])(va_list) = {
     [__NR_mremap] = sys_mremap,
     [__NR_madvise] = sys_madvise,
 };
+
+muslcsys_syscall_t muslcsys_install_syscall(int syscall, muslcsys_syscall_t new_syscall) {
+    if (syscall >= ARRAY_SIZE(syscall_table)) {
+        ZF_LOGF("Syscall %d exceeds syscall table size of %zu", syscall, ARRAY_SIZE(syscall_table));
+    }
+    muslcsys_syscall_t ret = syscall_table[syscall];
+    syscall_table[syscall] = new_syscall;
+    return ret;
+}
+
+/* Switch the thread syscalls from their boot variant to their regular
+ * default implementation. We do this at the lowest priority so that
+ * it can be overriden. We are able to have this constructor
+ * in this file since we know it will get looked at by the linker due
+ * to __vsyscall_ptr being here */
+static void CONSTRUCTOR(CONSTRUCTOR_MIN_PRIORITY) init_syscall_table(void) {
+    muslcsys_syscall_t ret UNUSED;
+    ret = muslcsys_install_syscall(__NR_set_tid_address, sys_set_tid_address);
+    assert(ret == boot_set_tid_address);
+#ifdef __NR_set_thread_area
+    ret = muslcsys_install_syscall(__NR_set_thread_area, sys_set_thread_area);
+    assert(ret == boot_set_thread_area);
+#endif
+}
 
 #ifdef CONFIG_DEBUG_BUILD
 static void debug_error(int sysnum) {

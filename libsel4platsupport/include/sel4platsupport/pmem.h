250d16f60c32433dc18898fa5f23fbef24083002
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2017-05-15 17:17:12 +1000

    Add sel4platsupport_get_pmem_region_list
    Returns a region list of the platform's memory layout

---
+++ libsel4platsupport/include/sel4platsupport/pmem.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(D61_BSD)
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include <simple/simple.h>
+
+typedef enum pmem_type {
+    PMEM_TYPE_RAM,
+    PMEM_TYPE_UNKNOWN,
+    PMEM_NUM_REGION_TYPES,
+} pmem_type_t;
+
+typedef struct pmem_region {
+    pmem_type_t type;
+    uint64_t base_addr;
+    uint64_t length;
+} pmem_region_t;
+
+/**
+ * Returns number of physical memory regions.  Each platform has a specific implementation
+ * @param  simple      libsel4simple implementation
+ * @return             Number of regions or -1 on error.
+ */
+int sel4platsupport_get_num_pmem_regions(simple_t *simple);
+
+/**
+ * Returns an array of physical memory regions.  Each platform has a specific implementation
+ * @param  simple      libsel4simple implementation
+ * @param  max_length  max length of region array
+ * @param  region_list pointer to region array
+ * @return             Number of regions or -1 on error.
+ */
+int sel4platsupport_get_pmem_region_list(simple_t *simple, size_t max_length, pmem_region_t *region_list);
---
+++ libsel4platsupport/src/arch/x86/pmem.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(D61_BSD)
+ */
+
+#include <stdint.h>
+#include <sel4/sel4.h>
+#include <sel4/arch/bootinfo_types.h>
+#include <sel4platsupport/pmem.h>
+#include <utils/util.h>
+
+int sel4platsupport_get_num_pmem_regions(simple_t *simple) {
+    seL4_X86_BootInfo_mmap_t data;
+    int error = simple_get_extended_bootinfo(simple, SEL4_BOOTINFO_HEADER_X86_MBMMAP, &data, sizeof(seL4_X86_BootInfo_mmap_t));
+    if (error == -1) {
+        ZF_LOGE("Could not find info");
+        return 0;
+    }
+
+    return data.mmap_length/sizeof(data.mmap[0]);
+}
+
+int sel4platsupport_get_pmem_region_list(simple_t *simple, size_t max_length, pmem_region_t *region_list) {
+    seL4_X86_BootInfo_mmap_t data;
+    int error = simple_get_extended_bootinfo(simple, SEL4_BOOTINFO_HEADER_X86_MBMMAP, &data, sizeof(seL4_X86_BootInfo_mmap_t));
+    if (error == -1) {
+        ZF_LOGE("Could not find info");
+        return -1;
+    }
+    seL4_X86_mb_mmap_t *mmap = (seL4_X86_mb_mmap_t *)((unsigned long)data.mmap);
+    size_t i = 0;
+    for (i = 0; i < max_length && i < (data.mmap_length/sizeof(mmap[0])); i++) {
+        region_list[i].type = mmap[i].type == SEL4_MULTIBOOT_RAM_REGION_TYPE ? PMEM_TYPE_RAM : PMEM_TYPE_UNKNOWN;
+        region_list[i].base_addr = mmap[i].base_addr;
+        region_list[i].length  = mmap[i].length;
+    }
+    return i;
+}

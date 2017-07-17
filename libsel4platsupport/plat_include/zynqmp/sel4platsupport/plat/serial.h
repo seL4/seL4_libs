f75f87b1445a9b6c265061f73d6a34aea88bcf13
Nathan Studer <nathan.studer@dornerworks.com>
2017-07-17 09:37:13 -0400

    Add zynqmp include based on zynq7000 ones

---
+++ libsel4platsupport/plat_include/zynqmp/sel4platsupport/plat/serial.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2017, DornerWorks
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_DORNERWORKS_BSD)
+ */
+/*
+ * This data was produced by DornerWorks, Ltd. of Grand Rapids, MI, USA under
+ * a DARPA SBIR, Contract Number D16PC00107.
+ *
+ * Approved for Public Release, Distribution Unlimited.
+ */
+#pragma once
+
+#include <platsupport/plat/serial.h>
+
+#define DEFAULT_SERIAL_PADDR UART0_PADDR
+#define DEFAULT_SERIAL_INTERRUPT UART0_IRQ
+

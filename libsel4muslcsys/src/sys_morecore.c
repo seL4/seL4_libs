921dc17b8a8560271d9ff1abb72ff1fe8c7e7328
Kofi Doku Atuah <kofidoku.atuah@data61.csiro.au>
2017-07-26 14:34:00 +1000

    Use vspace_new_pages_at() to resolve circular malloc() issue.
    Use vspace_new_pages_at() for mmap's dynamic implementation and manually
    update the position of the reservation.

--- libsel4muslcsys/src/sys_morecore.c
+++ libsel4muslcsys/src/sys_morecore.c
@@ -224,9 +224,28 @@ sys_mmap_impl_dynamic(void *addr, size_t length, int prot, int flags, int fd, of
     }
     if (flags & MAP_ANONYMOUS) {
         /* determine how many pages we need */
+        void *placement_vaddr;
         uint32_t pages = ROUND_UP(length, PAGE_SIZE_4K) / PAGE_SIZE_4K;
-        void *ret = vspace_new_pages(muslc_this_vspace, seL4_AllRights, pages, seL4_PageBits);
-        return (long)ret;
+
+        if (addr != NULL) {
+            placement_vaddr = addr;
+        } else {
+            if (brk_start == 0) {
+                brk_start = (uintptr_t)muslc_brk_reservation_start;
+            }
+            placement_vaddr = brk_start;
+        }
+
+        int error = vspace_new_pages_at_vaddr(muslc_this_vspace,
+                                              placement_vaddr,
+                                              pages, seL4_PageBits,
+                                              muslc_brk_reservation);
+        if (error != 0) {
+            return (long)NULL;
+        }
+
+        brk_start += pages * PAGE_SIZE_4K;
+        return vaddr;
     }
     assert(!"not implemented");
     return -ENOMEM;

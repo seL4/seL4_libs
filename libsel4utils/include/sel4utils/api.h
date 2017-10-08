ac3742da2658492d11c59bb561a524a51791631d
Adrian Danis <Adrian.Danis@data61.csiro.au>
2017-10-09 10:47:59 +1100

    sel4utils: Provides wrapper for creating guards
    With the removal of the seL4_CapData_t type in libsel4 it is tedious to created a guard
    correctly packed into a seL4_Word. This commit provides wrappers that follow the uprade
    notes in the kernel CHANGES file and provides a direct replacement for seL4_CapData_Guard_new
    without the need for .words[0]

--- libsel4utils/include/sel4utils/api.h
+++ libsel4utils/include/sel4utils/api.h
@@ -15,4 +15,20 @@
 /* This file provides various small API wrappers either for simple convenience, backwards compatibility,
    or abstracting different kernel versions */
 
+#include <sel4/sel4.h>
 #include <sel4utils/mcs_api.h>
+
+/* Helper for generating a guard. The guard itself is a bitpacked data structure, but is passed
+   to invocations as a raw word */
+static inline seL4_Word api_make_guard_word(seL4_Word guard, seL4_Word guard_size) {
+    /* the bitfield generated _new function will assert that our chosen values fit
+       into the datastructure so there is no need for us to do anything beyond
+       blindly pass them in */
+    return seL4_CNode_CapData_new(guard, guard_size).words[0];
+}
+
+/* Helper for making an empty guard. Typically a guard matches zeroes and is effectively acting
+   as a skip. This is a convenience wrapper and api_make_guard_word */
+static inline seL4_Word api_make_guard_skip_word(seL4_Word guard_size) {
+    return api_make_guard_word(0, guard_size);
+}

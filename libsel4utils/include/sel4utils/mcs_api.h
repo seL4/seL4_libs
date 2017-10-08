63967dc7f7f0163cd4b66352996badfab4a292aa
Adrian Danis <Adrian.Danis@data61.csiro.au>
2017-10-09 10:16:29 +1100

    sel4utils: Expand API wrapping helpers
    Changes 'api.h' to be more general in the kinds of API helpers it can contain, and moves the
    existing MCS specific helpers to their own file.

---
+++ libsel4utils/include/sel4utils/mcs_api.h
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#pragma once
+
+/* This file provides backwards compatible API wrappers for seemlessly switching between the MCS kernel
+   and non-MCS kernel without too many #ifdefs and without confusing cscope and other analysis tools.
+
+   Of course, if any of the new functionality of the MCS kernel is leveraged, the switch won't be
+   seemless.
+*/
+
+#include <autoconf.h>
+#include <sel4/sel4.h>
+#include <sel4utils/sel4_zf_logif.h>
+
+#ifdef CONFIG_KERNEL_RT
+#define CONFIG_TIMER_TICK_MS CONFIG_BOOT_THREAD_TIME_SLICE
+#else
+#define CONFIG_BOOT_THREAD_TIME_SLICE CONFIG_TIMER_TICK_MS
+#endif
+
+static inline seL4_MessageInfo_t api_wait(seL4_CPtr ep, seL4_Word *badge)
+{
+#ifdef CONFIG_KERNEL_RT
+    return seL4_Wait(ep, badge);
+#else
+    return seL4_Recv(ep, badge);
+#endif
+}
+
+static inline seL4_MessageInfo_t api_recv(seL4_CPtr ep, seL4_Word *badge, UNUSED seL4_CPtr reply)
+{
+#ifdef CONFIG_KERNEL_RT
+    return seL4_Recv(ep, badge, reply);
+#else
+    return seL4_Recv(ep, badge);
+#endif
+}
+
+static inline seL4_MessageInfo_t api_nbrecv(seL4_CPtr ep, seL4_Word *badge, UNUSED seL4_CPtr reply)
+{
+#ifdef CONFIG_KERNEL_RT
+    return seL4_NBRecv(ep, badge, reply);
+#else
+    return seL4_NBRecv(ep, badge);
+#endif
+}
+
+static inline void api_reply(UNUSED seL4_CPtr reply, seL4_MessageInfo_t msg)
+{
+#ifdef CONFIG_KERNEL_RT
+    seL4_Send(reply, msg);
+#else
+    seL4_Reply(msg);
+#endif
+}
+
+static inline seL4_MessageInfo_t api_reply_recv(seL4_CPtr ep, seL4_MessageInfo_t msg, seL4_Word *badge,
+        UNUSED seL4_CPtr reply)
+{
+#ifdef CONFIG_KERNEL_RT
+    return seL4_ReplyRecv(ep, msg, badge, reply);
+#else
+    return seL4_ReplyRecv(ep, msg, badge);
+#endif
+}
+
+static inline seL4_MessageInfo_t api_nbsend_recv(UNUSED seL4_CPtr send, UNUSED seL4_MessageInfo_t info,
+                                                 UNUSED seL4_CPtr recv, UNUSED seL4_Word *badge,
+                                                 UNUSED seL4_CPtr reply)
+{
+    ZF_LOGF_IF(!config_set(CONFIG_KERNEL_RT), "Not available on non MCS kernel");
+#ifdef CONFIG_KERNEL_RT
+    return seL4_NBSendRecv(send, info, recv, badge, reply);
+#else
+    return seL4_MessageInfo_new(0, 0, 0, 0);
+#endif
+}
+
+static inline seL4_MessageInfo_t api_nbsend_wait(UNUSED seL4_CPtr send, UNUSED seL4_MessageInfo_t info,
+                                                 UNUSED seL4_CPtr recv, UNUSED seL4_Word *badge)
+{
+    ZF_LOGF_IF(!config_set(CONFIG_KERNEL_RT), "Not available on non MCS kernel");
+#ifdef CONFIG_KERNEL_RT
+    return seL4_NBSendWait(send, info, recv, badge);
+#else
+    return seL4_MessageInfo_new(0, 0, 0, 0);
+#endif
+}
+
+static inline seL4_Error api_tcb_configure(seL4_CPtr tcb, seL4_CPtr ep, UNUSED seL4_CPtr timeout_ep,
+                                           seL4_PrioProps_t props, UNUSED seL4_CPtr sc, seL4_CPtr cspace,
+                                           seL4_CapData_t cdata, seL4_CPtr vspace, seL4_CapData_t vdata,
+                                           seL4_Word ipc_buffer_addr, seL4_CPtr ipc_buffer_cap)
+{
+#ifdef CONFIG_KERNEL_RT
+    return seL4_TCB_Configure(tcb, ep, timeout_ep, props, sc, cspace, cdata, vspace, vdata,
+                              ipc_buffer_addr, ipc_buffer_cap);
+#else
+    return seL4_TCB_Configure(tcb, ep, props, cspace, cdata, vspace, vdata,
+                              ipc_buffer_addr, ipc_buffer_cap);
+#endif
+}
+
+static inline seL4_Error api_tcb_set_space(seL4_CPtr tcb, seL4_CPtr ep, UNUSED seL4_CPtr timeout_ep,
+                                           seL4_CPtr cspace,
+                                           seL4_CapData_t cdata, seL4_CPtr vspace, seL4_CapData_t vdata)
+{
+#ifdef CONFIG_KERNEL_RT
+    return seL4_TCB_SetSpace(tcb, ep, timeout_ep, cspace, cdata, vspace, vdata);
+#else
+    return seL4_TCB_SetSpace(tcb, ep, cspace, cdata, vspace, vdata);
+#endif
+}
+
+static inline seL4_Error api_sc_bind(UNUSED seL4_CPtr sc, UNUSED seL4_CPtr tcb)
+{
+    if (!config_set(CONFIG_KERNEL_RT)) {
+        return (seL4_Error) -ENOSYS;
+    }
+#ifdef CONFIG_KERNEL_RT
+    return seL4_SchedContext_Bind(sc, tcb);
+#endif
+}
+
+static inline seL4_Error api_sc_unbind_object(UNUSED seL4_CPtr sc, UNUSED seL4_CPtr tcb)
+{
+    if (!config_set(CONFIG_KERNEL_RT)) {
+        return (seL4_Error) -ENOSYS;
+    }
+#ifdef CONFIG_KERNEL_RT
+    return seL4_SchedContext_UnbindObject(sc, tcb);
+#endif
+}
+
+static inline seL4_Error api_sc_unbind(UNUSED seL4_CPtr sc)
+{
+    if (!config_set(CONFIG_KERNEL_RT)) {
+        return (seL4_Error) -ENOSYS;
+    }
+#ifdef CONFIG_KERNEL_RT
+    return seL4_SchedContext_Unbind(sc);
+#endif
+}
+
+static inline seL4_Error api_sched_ctrl_configure(UNUSED seL4_CPtr sched_ctrl, UNUSED seL4_CPtr sc,
+                                                  UNUSED uint64_t budget, UNUSED uint64_t period,
+                                                  UNUSED seL4_Word refills, UNUSED seL4_Word badge)
+{
+    if (!config_set(CONFIG_KERNEL_RT)) {
+        return (seL4_Error) -ENOSYS;
+    }
+#ifdef CONFIG_KERNEL_RT
+    return seL4_SchedControl_Configure(sched_ctrl, sc, budget, period, refills, badge);
+#endif
+}

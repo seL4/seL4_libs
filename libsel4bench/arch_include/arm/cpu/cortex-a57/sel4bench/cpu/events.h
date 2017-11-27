572acb04a45b01430177bf76faedeac4fa1b6074
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-11-27 11:25:04 +1100

    Cortex-A57: add events.h
    - allows sel4bench to build for TX1
    - have checked events are correct according to Cortex-A57 manual

---
+++ libsel4bench/arch_include/arm/cpu/cortex-a57/sel4bench/cpu/events.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2017, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+#pragma once
+#define SEL4BENCH_EVENT_BUS_ACCESS_LD         0x60
+#define SEL4BENCH_EVENT_BUS_ACCESS_ST         0x61
+#define SEL4BENCH_EVENT_BR_INDIRECT_SPEC      0x7A
+#define SEL4BENCH_EVENT_EXC_IRQ               0x86
+#define SEL4BENCH_EVENT_EXC_FIQ               0x87

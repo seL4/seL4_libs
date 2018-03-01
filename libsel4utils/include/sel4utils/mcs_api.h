239f6dc6364c3bfd40e72bf3efdc96360d86a744
Anna Lyons <Anna.Lyons@data61.csiro.au>
2018-03-01 15:57:32 +1100

    Provide ep to seL4_TCB_SetSchedParams for RT

--- libsel4utils/include/sel4utils/mcs_api.h
+++ libsel4utils/include/sel4utils/mcs_api.h
@@ -122,10 +122,10 @@ static inline seL4_Error api_tcb_configure(seL4_CPtr tcb, seL4_CPtr ep, UNUSED s
 }
 
 static inline seL4_Error api_tcb_set_sched_params(seL4_CPtr tcb, seL4_CPtr auth, seL4_Word prio,
-                                                  seL4_Word mcp, seL4_CPtr sc)
+                                                  seL4_Word mcp, seL4_CPtr sc, seL4_CPtr ep)
 {
 #ifdef CONFIG_KERNEL_RT
-    return seL4_TCB_SetSchedParams(tcb, auth, mcp, prio, sc);
+    return seL4_TCB_SetSchedParams(tcb, auth, mcp, prio, sc, ep);
 #else
     return seL4_TCB_SetSchedParams(tcb, auth, mcp, prio);
 #endif

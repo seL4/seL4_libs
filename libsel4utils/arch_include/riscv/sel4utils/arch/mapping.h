58b93be8db6618f0e677869d5ec129e529a8f759
Adrian Danis <Adrian.Danis@data61.csiro.au>
2018-04-04 16:13:47 +1000

    libsel4utils: Correctly implement sel4utils_create_object_at_level for riscv
    This function just needs to create page tables whenever invoked, as that is always the
    object.

--- libsel4utils/arch_include/riscv/sel4utils/arch/mapping.h
+++ libsel4utils/arch_include/riscv/sel4utils/arch/mapping.h
@@ -20,34 +20,34 @@ static inline int
 sel4utils_create_object_at_level(vka_t *vka, seL4_Word failed_bits, vka_object_t *objects, int *num_objects, void *vaddr, seL4_CPtr vspace_root)
 {
     int error;
-    if (failed_bits == SEL4_MAPPING_LOOKUP_NO_LVL2PT) {
-        vka_object_t object;
-        error = vka_alloc_page_table(vka, &object);
-        if (error) {
-            printf("Failed to allocate page table");
-            return error;
-        }
-        error = seL4_RISCV_PageTable_Map(object.cptr, vspace_root, (seL4_Word)vaddr, seL4_RISCV_Default_VMAttributes);
-        if (error == seL4_DeleteFirst) {
-            /* through creating the object we must have ended up mapping this
-             * level as part of the metadata creation. Delete this and keep
-             * on going */
-            vka_free_object(vka, &object);
-            printf("Failed to map lvl2 page table, already exist, delete first");
-            return 0;
-        }
-        if (error) {
-            printf("Failed to map lvl2 page table");
-            vka_free_object(vka, &object);
-        } else {
-            objects[*num_objects] = object;
-            (*num_objects)++;
-        }
-        return error;
-    } else {
-        printf("Invalid lookup level %d", (int)failed_bits);
+    // ensure we aren't trying to create an object at a level where there can only be frames
+    if (failed_bits < SEL4_MAPPING_LOOKUP_NO_PT) {
+        ZF_LOGE("Invalid lookup level %d", (int)failed_bits);
         return seL4_InvalidArgument;
     }
+    // riscv only has one kind of object (page table) so all we need to do is put one of those in
+    vka_object_t object;
+    error = vka_alloc_page_table(vka, &object);
+    if (error) {
+        ZF_LOGE("Failed to allocate page table");
+        return error;
+    }
+    error = seL4_RISCV_PageTable_Map(object.cptr, vspace_root, (seL4_Word)vaddr, seL4_RISCV_Default_VMAttributes);
+    if (error == seL4_DeleteFirst) {
+        /* through creating the object we must have ended up mapping this
+         * level as part of the metadata creation. Delete this and keep
+         * on going */
+        vka_free_object(vka, &object);
+        return 0;
+    }
+    if (error) {
+        ZF_LOGE("Failed to map page table");
+        vka_free_object(vka, &object);
+    } else {
+        objects[*num_objects] = object;
+        (*num_objects)++;
+    }
+    return error;
 }
 
 #endif /* _SEL4UTILS_ARCH_MAPPING_H */

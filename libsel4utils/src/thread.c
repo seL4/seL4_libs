7beb31cfa781f921c9cd64420f449d06535f9e16
Anna Lyons <Anna.Lyons@data61.csiro.au>
2018-04-04 15:28:42 +1000

    SELFOUR-1295: add FSR print back to thread.c

--- libsel4utils/src/thread.c
+++ libsel4utils/src/thread.c
@@ -230,6 +230,7 @@ sel4utils_print_fault_message(seL4_MessageInfo_t tag, const char *thread_name)
                seL4_Fault_VMFault_get_PrefetchFault(fault) ? "prefetch fault" : "fault",
                (void*)seL4_Fault_VMFault_get_IP(fault),
                (void*)seL4_Fault_VMFault_get_Addr(fault),
+               (void *)seL4_Fault_VMFault_get_FSR(fault),
                COLOR_NORMAL);
         break;
 

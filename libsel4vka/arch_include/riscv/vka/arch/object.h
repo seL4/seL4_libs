ca6057e0f949538a04741ed7c4ea7d898b36aea8
Anna Lyons <Anna.Lyons@data61.csiro.au>
2018-04-04 15:19:44 +1000

    SELFOUR-1300: riscv remove vka_alloc_pd

--- libsel4vka/arch_include/riscv/vka/arch/object.h
+++ libsel4vka/arch_include/riscv/vka/arch/object.h
@@ -16,14 +16,9 @@
 #include <vka/kobject_t.h>
 #include <utils/util.h>
 
-static inline int vka_alloc_pd(vka_t *vka, vka_object_t *result)
-{
-    return vka_alloc_object(vka, kobject_get_type(KOBJECT_PAGE_TABLE, 0), seL4_PageTableBits, result);
-}
-
 static inline int vka_alloc_vspace_root(vka_t *vka, vka_object_t *result)
 {
-    return vka_alloc_pd(vka, result);
+    return vka_alloc_object(vka, kobject_get_type(KOBJECT_PAGE_TABLE, 0), seL4_PageTableBits, result);
 }
 
 

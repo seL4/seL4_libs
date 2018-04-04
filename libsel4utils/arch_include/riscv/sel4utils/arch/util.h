9a2af0e33417743ef877f736752c57e7f18cb62a
Anna Lyons <Anna.Lyons@data61.csiro.au>
2018-04-04 14:15:44 +1000

    SELFOUR-1283: fix arch/sel4_arch sel4utils
    - delete empty files and symlinks
    - merge riscv sel4_arch and arch util.h

--- libsel4utils/arch_include/riscv/sel4utils/arch/util.h
+++ libsel4utils/arch_include/riscv/sel4utils/arch/util.h
@@ -16,13 +16,39 @@
 #include <sel4/sel4.h>
 #include <sel4/arch/pfIPC.h>
 #include <sel4/arch/exIPC.h>
-#include <sel4utils/sel4_arch/util.h>
+
+#define EXCEPT_IPC_SYS_MR_IP EXCEPT_IPC_SYS_MR_RIP
+#define ARCH_SYSCALL_INSTRUCTION_SIZE 4
 
 static inline int
 sel4utils_is_read_fault(void)
 {
     seL4_Word fsr = seL4_GetMR(seL4_VMFault_FSR);
     return (fsr == 1 || fsr == 2 || fsr == 5);
+}
+
+static inline void
+sel4utils_set_instruction_pointer(seL4_UserContext *regs, seL4_Word value)
+{
+    regs->pc = value;
+}
+
+static inline seL4_Word
+sel4utils_get_instruction_pointer(seL4_UserContext regs)
+{
+    return regs.pc;
+}
+
+static inline seL4_Word
+sel4utils_get_sp(seL4_UserContext regs)
+{
+    return regs.sp;
+}
+
+static inline void
+sel4utils_set_stack_pointer(seL4_UserContext *regs, seL4_Word value)
+{
+    regs->sp = value;
 }
 
 #endif /* _SEL4UTILS_ARCH_UTIL_H */

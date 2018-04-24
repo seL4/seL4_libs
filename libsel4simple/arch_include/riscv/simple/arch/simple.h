1697cb16ecbc7820cbda78d7c7c1896e884195a1
Adrian Danis <Adrian.Danis@data61.csiro.au>
2018-04-24 15:50:38 +1000

    libsel4simple: Update arch_simple_getIOport_cap on riscv

--- libsel4simple/arch_include/riscv/simple/arch/simple.h
+++ libsel4simple/arch_include/riscv/simple/arch/simple.h
@@ -35,9 +35,9 @@ typedef struct arch_simple {
     arch_simple_get_IRQ_control_fn irq;
 } arch_simple_t;
 
-static inline seL4_CPtr 
-arch_simple_get_IOPort_cap(UNUSED arch_simple_t *arch_simple, UNUSED uint16_t start_port, UNUSED uint16_t end_port)
-{
-    ZF_LOGE("%s not implemented", __FUNCTION__);
-    return seL4_CapNull;
+static inline seL4_Error
+arch_simple_get_IOPort_cap(UNUSED arch_simple_t *simple, UNUSED uint16_t start_port,
+                           UNUSED uint16_t end_port, UNUSED seL4_Word root, UNUSED seL4_Word dest, UNUSED seL4_Word depth) {
+    ZF_LOGF("Calling get_IOPort_cap on arm!");
+    return seL4_IllegalOperation;
 }

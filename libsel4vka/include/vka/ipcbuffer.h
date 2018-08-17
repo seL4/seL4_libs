bb21cb1dc518a00bad95abc477fea84679b00b26
Thibaut Perami <thibaut.perami@data61.csiro.au>
2018-08-17 16:19:30 +1000

    libsel4vka: add function for setting the receive cap slot with a path

---
+++ libsel4vka/include/vka/ipcbuffer.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2018, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+
+#pragma once
+
+/* This file provides convenient wrappers around seL4 IPC buffer manipulation */
+#include <autoconf.h>
+#include <vka/cspacepath_t.h>
+#include <vka/object.h>
+
+static inline void
+vka_set_cap_receive_path(const cspacepath_t* src)
+{
+    seL4_SetCapReceivePath(
+               /* _service */      src->root,
+               /* index */         src->capPtr,
+               /* depth */         src->capDepth
+           );
+}

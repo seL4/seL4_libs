c9a10a0fa1f3d59df5609d525b0c613cd1120d42
Anna Lyons <Anna.Lyons@data61.csiro.au>
2019-06-18 16:37:58 +1000

    trivial: #ifdef iospace for riscv

--- libsel4utils/src/mapping.c
+++ libsel4utils/src/mapping.c
@@ -73,6 +73,7 @@ int sel4utils_map_page(vka_t *vka, seL4_CPtr vspace_root, seL4_CPtr frame, void 
                     cacheable, objects, num_objects);
 }
 
+#ifndef CONFIG_ARCH_RISCV
 int sel4utils_map_iospace_page(vka_t *vka, seL4_CPtr iospace, seL4_CPtr frame, seL4_Word vaddr,
                                seL4_CapRights_t rights, int cacheable, seL4_Word size_bits,
                                vka_object_t *pts, int *num_pts)
@@ -80,7 +81,7 @@ int sel4utils_map_iospace_page(vka_t *vka, seL4_CPtr iospace, seL4_CPtr frame, s
     return map_page(vka, vspace_iospace_map_page, vspace_get_iospace_map_obj, iospace, frame, (void *) vaddr, rights,
                     cacheable, pts, num_pts);
 }
-
+#endif /* CONFIG_ARCH_RISCV */
 #ifdef CONFIG_VTX
 
 /*map a frame into guest os's physical address space*/

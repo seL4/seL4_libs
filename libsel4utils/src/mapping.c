seL4/seL4_libs

commit 3995c85d36ddf5aeb9373fc4d5445ea6feca6a24

sel4utils: use map objects for all page types
Reduce redundant code in mapping for vspace/io/ept page(table)s

libsel4utils/src/mapping.c
@@ -20,9 +20,9 @@
 #include <sel4utils/util.h>
 #include <vspace/mapping.h>
 
-int
-sel4utils_map_page(vka_t *vka, seL4_CPtr vspace_root, seL4_CPtr frame, void *vaddr,
-                   seL4_CapRights_t rights, int cacheable, vka_object_t *objects, int *num_objects)
+static int map_page(vka_t *vka, vspace_map_page_fn_t map_page_fn, vspace_get_map_obj_fn map_obj_fn,
+        seL4_CPtr root, seL4_CPtr frame, void *vaddr, seL4_CapRights_t rights,
+        int cacheable, vka_object_t *objects, int *num_objects)
 {
     if (!vka || !vaddr || !objects || !num_objects || !root) {
         return EINVAL;
@@ -32,10 +32,10 @@ sel4utils_map_page(vka_t *vka, seL4_CPtr vspace_root, seL4_CPtr frame, void *vad
                                   seL4_ARCH_Uncached_VMAttributes;
 
     *num_objects = 0;
-    int error = seL4_ARCH_Page_Map(frame, vspace_root, (seL4_Word) vaddr, rights, attr);
+    int error = map_page_fn(frame, root, (seL4_Word) vaddr, rights, attr);
     while (error == seL4_FailedLookup) {
         vspace_map_obj_t obj = {0};
-        error = vspace_get_map_obj(seL4_MappingFailedLookupLevel(), &obj);
+        error = map_obj_fn(seL4_MappingFailedLookupLevel(), &obj);
         /* we should not get an incorrect values for seL4_MappingFailedLookupLevel */
         assert(error == 0);
 
@@ -45,7 +45,7 @@ sel4utils_map_page(vka_t *vka, seL4_CPtr vspace_root, seL4_CPtr frame, void *vad
             return error;
         }
 
-        error = vspace_map_obj(&obj, objects[*num_objects].cptr, vspace_root,
+        error = vspace_map_obj(&obj, objects[*num_objects].cptr, root,
                                (seL4_Word)vaddr, seL4_ARCH_Default_VMAttributes);
         if (error == seL4_DeleteFirst) {
             /* this is the case where the allocation of the page table needed to map in
@@ -58,7 +58,7 @@ sel4utils_map_page(vka_t *vka, seL4_CPtr vspace_root, seL4_CPtr frame, void *vad
                 return error;
             }
         }
-        error = seL4_ARCH_Page_Map(frame, vspace_root, (seL4_Word) vaddr, rights, attr);
+        error = map_page_fn(frame, root, (seL4_Word) vaddr, rights, attr);
     }
     if (error != seL4_NoError) {
         ZF_LOGE("Failed to map page at address %p with cap %"PRIuPTR", error: %d", vaddr, frame, error);
@@ -66,42 +66,21 @@ sel4utils_map_page(vka_t *vka, seL4_CPtr vspace_root, seL4_CPtr frame, void *vad
     return error;
 }
 
-#if defined(CONFIG_IOMMU) || defined(CONFIG_ARM_SMMU)
+int sel4utils_map_page(vka_t *vka, seL4_CPtr vspace_root, seL4_CPtr frame, void *vaddr,
+                       seL4_CapRights_t rights, int cacheable, vka_object_t *objects, int *num_objects)
+{
+    return map_page(vka, seL4_ARCH_Page_Map, vspace_get_map_obj, vspace_root, frame, vaddr, rights,
+                    cacheable, objects, num_objects);
+}
+
 int
 sel4utils_map_iospace_page(vka_t *vka, seL4_CPtr iospace, seL4_CPtr frame, seL4_Word vaddr,
                            seL4_CapRights_t rights, int cacheable, seL4_Word size_bits,
                            vka_object_t *pts, int *num_pts)
 {
-    int error;
-    assert(vka);
-    assert(iospace);
-    assert(frame);
-    assert(cacheable);
-    assert(size_bits == seL4_PageBits);
-    /* Start mapping in pages and page tables until it bloody well works */
-    int num = 0;
-    while ( (error = seL4_ARCH_Page_MapIO(frame, iospace, rights, vaddr)) == seL4_FailedLookup && error != seL4_NoError) {
-        /* Need a page table? */
-        vka_object_t pt;
-        error = vka_alloc_io_page_table(vka, &pt);
-        if (error) {
-            return error;
-        }
-        if (pts) {
-            pts[num] = pt;
-            num++;
-            assert(num_pts);
-            *num_pts = num;
-        }
-        error = seL4_ARCH_IOPageTable_Map(pt.cptr, iospace, vaddr);
-        if (error != seL4_NoError) {
-            return error;
-        }
-    }
-    return error;
+    return map_page(vka, vspace_iospace_map_page, vspace_get_iospace_map_obj, iospace, frame, (void *) vaddr, rights,
+                    cacheable, pts, num_pts);
 }
-
-#endif /* defined(CONFIG_IOMMU) || defined(CONFIG_ARM_SMMU) */
 
 #ifdef CONFIG_VTX
 
@@ -111,108 +90,13 @@ sel4utils_map_ept_page(vka_t *vka, seL4_CPtr pd, seL4_CPtr frame, seL4_Word vadd
                        seL4_CapRights_t rights, int cacheable, seL4_Word size_bits,
                        vka_object_t *pagetable, vka_object_t *pagedir, vka_object_t *pdpt)
 {
-    int ret;
-
-    assert(vka);
-    assert(pd);
-    assert(frame);
-    assert(size_bits);
-    assert(pagetable);
-    assert(pagedir);
-
-    seL4_ARCH_VMAttributes attr = 0;
-
-    if (!cacheable) {
-        attr = seL4_X86_CacheDisabled;
-    }
-
-    /* Try map into EPT directory first. */
-    ret = seL4_X86_Page_MapEPT(frame, pd, vaddr, rights, attr);
-    if (ret == seL4_NoError) {
-        /* Successful! */
-        return ret;
-    }
-    if (ret != seL4_FailedLookup) {
-        ZF_LOGE("Failed to map page, error: %d", ret);
-        return ret;
-    }
-
-    if (size_bits != seL4_PageBits) {
-        /* Allocate a directory table for a large page entry. */
-        ret = vka_alloc_ept_page_directory(vka, pagedir);
-        if (ret != seL4_NoError) {
-            ZF_LOGE("Allocation of EPT page directory failed, error: %d", ret);
-            return ret;
-        }
-
-        /* Map in the page directory. */
-        ret = seL4_X86_EPTPD_Map(pagedir->cptr, pd, vaddr, attr);
-
-        if (ret != seL4_NoError) {
-            ZF_LOGE("Failed to map EPT PD, error: %d", ret);
-            return ret;
-        }
-    } else {
-        /* Allocate an EPT page table for a 4K entry. */
-        ret = vka_alloc_ept_page_table(vka, pagetable);
-        if (ret != seL4_NoError) {
-            ZF_LOGE("allocating ept page table failed");
-            return ret;
-        }
-
-        /* Map in the EPT page table. */
-        ret = seL4_X86_EPTPT_Map(pagetable->cptr, pd, vaddr, attr);
-
-        /* Mapping could fail due to missing PD. In this case, we map the PD and try again. */
-        if (ret == seL4_FailedLookup) {
-            /* Allocate an EPT page directory for the page table. */
-            ret = vka_alloc_ept_page_directory(vka, pagedir);
-            if (ret != seL4_NoError) {
-                ZF_LOGE("Allocating EPT page directory failed");
-                return ret;
-            }
-
-            /* Map the EPT page directory in. */
-            ret = seL4_X86_EPTPD_Map(pagedir->cptr, pd, vaddr, attr);
-            if (ret == seL4_FailedLookup) {
-                /* Could be a missing PDPT */
-                ret = vka_alloc_ept_pdpt(vka, pdpt);
-                if (ret != seL4_NoError) {
-                    ZF_LOGE("allocating ept pdpt failed");
-                    return ret;
-                }
-                ret = seL4_X86_EPTPDPT_Map(pdpt->cptr, pd, vaddr, attr);
-                if (ret != seL4_NoError) {
-                    ZF_LOGE("Failed to map EPT PDPT, error: %d", ret);
-                    return ret;
-                }
-                /* Try to map the page directory in again */
-                ret = seL4_X86_EPTPD_Map(pagedir->cptr, pd, vaddr, attr);
-            }
-
-            if (ret != seL4_NoError) {
-                ZF_LOGE("Failed to map EPT PD, error: %d", ret);
-                return ret;
-            }
-
-            /* Try to map the page table again. */
-            ret = seL4_X86_EPTPT_Map(pagetable->cptr, pd, vaddr, attr);
-            if (ret != seL4_NoError) {
-                ZF_LOGE("Second attempt at mapping EPT PT failed, error: %d", ret);
-                return ret;
-            }
-        } else if (ret) {
-            ZF_LOGE("EPT PT failed to map, error: %d", ret);
-            return ret;
-        }
-    }
-
-    if (ret != seL4_NoError) {
-        return ret;
-    }
-
-    /* Try to map the frame again. */
-    return seL4_X86_Page_MapEPT(frame, pd, vaddr, rights, attr);
+    vka_object_t objects[3];
+    int num_objects;
+    int error = map_page(vka, seL4_X86_Page_MapEPT, vspace_get_ept_map_obj, pd, frame vaddr, rights, cacheable, &objects, &num_objects);
+    *pagetable = objects[0];
+    *pagedir = objects[1];
+    *pdpt = objects[2];
+    return error;
 }
 
 #endif /* CONFIG_VTX */

commit 39d5a11ba7ae0b9433c7ba5f8f09ff5b5d022fa9

libsel4utils: fix compile errors in mapping.c
introduced in 3995c85d36ddf5aeb9373fc4d5445ea6feca6a24

libsel4utils/src/mapping.c
@@ -91,7 +91,7 @@ int sel4utils_map_ept_page(vka_t *vka, seL4_CPtr pd, seL4_CPtr frame, seL4_Word 
 {
     vka_object_t objects[3];
     int num_objects;
-    int error = map_page(vka, seL4_X86_Page_MapEPT, vspace_get_ept_map_obj, pd, frame vaddr, rights, cacheable, &objects,
+    int error = map_page(vka, seL4_X86_Page_MapEPT, vspace_get_ept_map_obj, pd, frame, (void *) vaddr, rights, cacheable, objects,
                          &num_objects);
     *pagetable = objects[0];
     *pagedir = objects[1];

libsel4vspace/src/arch/x86/mapping.c
@@ -48,7 +48,7 @@ int vspace_get_ept_map_obj(seL4_Word failed_bits, vspace_map_obj_t *obj)
         obj->map_fn = seL4_X86_EPTPD_Map;
         return 0;
     case SEL4_MAPPING_LOOKUP_NO_PDPT:
-        obj->size_bits = seL4_EPTPDPTBits;
+        obj->size_bits = seL4_X86_EPTPDPTBits;
         obj->type = seL4_X86_EPTPDPTObject;
         obj->map_fn = seL4_X86_EPTPDPT_Map;
         return 0;
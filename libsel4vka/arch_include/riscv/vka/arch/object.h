caa1dc08d06be727da07801528187a73ed89b3a4
Yanyan Shen <yanyan.shen@data61.csiro.au>
2019-07-26 07:45:06 +1000

    libsel4vka: Add VCPU alloc for RISCV
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- libsel4vka/arch_include/riscv/vka/arch/object.h
+++ libsel4vka/arch_include/riscv/vka/arch/object.h
@@ -9,6 +9,13 @@
 #include <vka/kobject_t.h>
 #include <utils/util.h>
 
+#ifdef CONFIG_RISCV_HE
+static inline int vka_alloc_vcpu(vka_t *vka, vka_object_t *result)
+{
+    return vka_alloc_object(vka, seL4_RISCV_VCPUObject, seL4_RISCV_VCPUBits, result);
+}
+#endif
+
 static inline int vka_alloc_vspace_root(vka_t *vka, vka_object_t *result)
 {
     return vka_alloc_object(vka, kobject_get_type(KOBJECT_PAGE_TABLE, 0), seL4_PageTableBits, result);
@@ -33,10 +40,13 @@ vka_arch_get_object_size(seL4_Word objectType)
 #endif
     case seL4_RISCV_PageTableObject:
         return seL4_PageTableBits;
+#ifdef CONFIG_RISCV_HE
+    case seL4_RISCV_VCPUObject:
+        return seL4_RISCV_VCPUBits;
+#endif
 
     default:
          ZF_LOGE("Unknown object type %ld", (long)objectType);
          return -1;
     }
 }
-

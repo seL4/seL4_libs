43c8d16371c0bd78d847d16143a9e885d9cf886e
Axel Heider <axelheider@gmx.de>
2022-01-02 11:39:55 +0100

    libsel4debug: use proper closing bracket
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- libsel4debug/src/bootinfo.c
+++ libsel4debug/src/bootinfo.c
@@ -18,11 +18,11 @@ void debug_print_bootinfo(seL4_BootInfo *info)
     printf("Node %lu of %lu\n", (long)info->nodeID, (long)info->numNodes);
     printf("IOPT levels:     %u\n", (int)info->numIOPTLevels);
     printf("IPC buffer:      %p\n", info->ipcBuffer);
-    printf("Empty slots:     [%lu --> %lu)\n", (long)info->empty.start, (long)info->empty.end);
-    printf("sharedFrames:    [%lu --> %lu)\n", (long)info->sharedFrames.start, (long)info->sharedFrames.end);
-    printf("userImageFrames: [%lu --> %lu)\n", (long)info->userImageFrames.start, (long)info->userImageFrames.end);
-    printf("userImagePaging: [%lu --> %lu)\n", (long)info->userImagePaging.start, (long)info->userImagePaging.end);
-    printf("untypeds:        [%lu --> %lu)\n", (long)info->untyped.start, (long)info->untyped.end);
+    printf("Empty slots:     [%lu --> %lu]\n", (long)info->empty.start, (long)info->empty.end);
+    printf("sharedFrames:    [%lu --> %lu]\n", (long)info->sharedFrames.start, (long)info->sharedFrames.end);
+    printf("userImageFrames: [%lu --> %lu]\n", (long)info->userImageFrames.start, (long)info->userImageFrames.end);
+    printf("userImagePaging: [%lu --> %lu]\n", (long)info->userImagePaging.start, (long)info->userImagePaging.end);
+    printf("untypeds:        [%lu --> %lu]\n", (long)info->untyped.start, (long)info->untyped.end);
     printf("Initial thread domain: %u\n", (int)info->initThreadDomain);
     printf("Initial thread cnode size: %u\n", (int)info->initThreadCNodeSizeBits);
     printf("List of untypeds\n");

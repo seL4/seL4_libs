41d2e61d3ace5f5cd72453b14f56743a881a9e29
Jimmy Brush <code@jimmah.com>
2023-08-18 03:26:49 -0400

    libsel4simple-default,riscv: implement irq_trigger
    Wire up simple->irq_trigger to seL4_IRQControl_GetTrigger on risc-v.
    This allows risc-v drivers in libplatsupport to register edge-triggered
    interrupts.
    
    Signed-off-by: Jimmy Brush <code@jimmah.com>

--- libsel4simple-default/src/arch/riscv/default.c
+++ libsel4simple-default/src/arch/riscv/default.c
@@ -15,14 +15,21 @@
 
 #include <vspace/page.h>
 
-seL4_Error simple_default_get_irq(void *data, int irq, seL4_CNode root, seL4_Word index, uint8_t depth) {
+seL4_Error simple_default_get_irq(void *data, int irq, seL4_CNode root, seL4_Word index, uint8_t depth)
+{
     return seL4_IRQControl_Get(seL4_CapIRQControl, irq, root, index, depth);
 }
 
-void
-simple_default_init_arch_simple(arch_simple_t *simple, void *data) 
+seL4_Error simple_default_get_irq_trigger(void *data, int irq, int trigger, UNUSED int core, seL4_CNode root,
+                                          seL4_Word index, uint8_t depth)
+{
+    return seL4_IRQControl_GetTrigger(seL4_CapIRQControl, irq, trigger, root, index, depth);
+}
+
+void simple_default_init_arch_simple(arch_simple_t *simple, void *data) 
 {
     simple->data = data;
     simple->irq = simple_default_get_irq;
+    simple->irq_trigger = simple_default_get_irq_trigger;
 }
 
